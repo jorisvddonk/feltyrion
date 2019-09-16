a3fab1b848697d501c2b601d5985b9c0b4158d08
joris <jorisvddonk@gmail.com>
2019-09-16 20:03:01 +0200

    search star by name

--- GQLStar.resolver.ts
+++ GQLStar.resolver.ts
@@ -1,13 +1,20 @@
 import { Query, Arg, Resolver } from "type-graphql";
 import Starmap from "./Starmap";
 import { GQLStar } from "./GQLStar";
+import { Star } from "noctis-starmap";
 
 @Resolver(GQLStar)
 export class GQLStarResolver {
   constructor() {}
 
   @Query(returns => GQLStar)
-  async star(@Arg("name") name: string) {}
+  async star(@Arg("name") name: string) {
+    const star: Star | undefined = Starmap.getStarByName(name);
+    if (star === undefined) {
+      throw new Error("Star not found!");
+    }
+    return star;
+  }
 
   @Query(returns => [GQLStar])
   async stars() {
